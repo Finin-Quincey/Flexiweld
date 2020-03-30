94cd378a63b72fe7775b7aa30f2a07dbfe39746f
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2020-03-30 11:57:43 +0100

    Initial commit

---
+++ src/uob/flexiweld/LineDetectorTest.java
@@ -0,0 +1,147 @@
+package uob.flexiweld;
+
+import org.opencv.core.*;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+
+import java.util.function.BiConsumer;
+
+public class LineDetectorTest {
+
+	public static final int CAMERA_NUMBER = 0;
+	public static final String WINDOW_NAME = "Line Detection Test";
+
+	public static final int WIDTH = 1920;
+	public static final int HEIGHT = 1080;
+
+	public static final int FRAMERATE = 30; // Target framerate in frames per second
+
+	public static final Scalar ANNOTATION_COLOUR = new Scalar(0, 255, 0); // This is blue-green-red for some reason
+
+	public static void main(String[] args){
+
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//		Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+//		System.out.println("mat = " + mat.dump());
+
+		VideoCapture vc = new VideoCapture();
+		vc.open(CAMERA_NUMBER);
+		System.out.println(vc.isOpened() ? "Video capture opened successfully" : "Failed to open video capture");
+
+		Mat frame = new Mat();
+		Mat edges;
+
+		vc.read(frame);
+
+		float scaleFactor = Math.min(WIDTH/frame.width(), HEIGHT/frame.height());
+		int newWidth = (int)(frame.width() * scaleFactor);
+		int newHeight = (int)(frame.height() * scaleFactor);
+		final Size size = new Size(newWidth, newHeight);
+
+		final Mat mapX = new Mat(size, CvType.CV_32F);
+		final Mat mapY = new Mat(size, CvType.CV_32F);
+
+		final Point textPt0 = new Point(10, 30);
+		final Point textPt1 = new Point(10, 100);
+		final Point textPt2 = new Point(10, newHeight - 90);
+		final Point textPt3 = new Point(10, newHeight - 60);
+		final Point textPt4 = new Point(10, newHeight - 30);
+
+		setupFlipMatrices(mapX, mapY);
+
+		HighGui.namedWindow(WINDOW_NAME, HighGui.WINDOW_AUTOSIZE);
+		//HighGui.resizeWindow(WINDOW_NAME, WIDTH, HEIGHT);
+
+		long time;
+		boolean displayEdges = false;
+		boolean displayLines = true;
+		boolean mirror = true;
+
+		while(true){
+
+			time = System.currentTimeMillis();
+
+			vc.read(frame);
+
+			frame = process(frame, (s, d) -> Imgproc.resize(s, d, size));
+			if(mirror) frame = process(frame, (s, d) -> Imgproc.remap(s, d, mapX, mapY, Imgproc.INTER_LINEAR));
+
+			// Edge detection
+			edges = process(frame, (s, d) -> Imgproc.Canny(s, d, 50, 200, 3, false));
+
+			if(displayEdges) frame = process(edges, (s, d) -> Imgproc.cvtColor(s, d, Imgproc.COLOR_GRAY2BGR));
+
+			if(displayLines){
+				// Probabilistic Hough Line Transform
+				Mat lines = new Mat(); // will hold the results of the detection
+				Imgproc.HoughLinesP(edges, lines, 1, Math.PI / 180, 50, 50, 10); // runs the actual detection
+				// Draw the lines
+				for(int x = 0; x < lines.rows(); x++){
+					double[] l = lines.get(x, 0);
+					Imgproc.line(frame, new Point(l[0], l[1]), new Point(l[2], l[3]), ANNOTATION_COLOUR, 2, Imgproc.LINE_AA, 0);
+				}
+			}
+
+			long frameTime = System.currentTimeMillis() - time;
+			float fps = 1000f/frameTime;
+
+			Imgproc.putText(frame, "Press Esc to close", textPt0, Core.FONT_HERSHEY_PLAIN, 2, ANNOTATION_COLOUR);
+			Imgproc.putText(frame, String.format("%.2f fps", fps), textPt1, Core.FONT_HERSHEY_PLAIN, 2, ANNOTATION_COLOUR);
+			Imgproc.putText(frame, String.format("Edges %s (E to toggle)", displayEdges ? "on" : "off"), textPt2, Core.FONT_HERSHEY_PLAIN, 2, ANNOTATION_COLOUR);
+			Imgproc.putText(frame, String.format("Lines %s (L to toggle)", displayLines ? "on" : "off"), textPt3, Core.FONT_HERSHEY_PLAIN, 2, ANNOTATION_COLOUR);
+			Imgproc.putText(frame, String.format("Mirror %s (M to toggle)", mirror ? "on" : "off"), textPt4, Core.FONT_HERSHEY_PLAIN, 2, ANNOTATION_COLOUR);
+
+			HighGui.imshow(WINDOW_NAME, frame);
+
+			//System.out.println(1000/FRAMERATE - (int)frameTime);
+			int key = HighGui.waitKey(Math.max(5, 1000/FRAMERATE - (int)frameTime));
+			if(key == 27) break; // Esc to close
+			if(key == 69) displayEdges = !displayEdges; // E to toggle edges
+			if(key == 76) displayLines = !displayLines; // L to toggle line detector
+			if(key == 77) mirror = !mirror; // M to toggle mirror
+
+		}
+
+		HighGui.destroyAllWindows();
+		System.exit(0);
+	}
+
+	private static void setupFlipMatrices(Mat mapX, Mat mapY){
+		// TODO: Figure out what this is actually doing!
+		// (I know what it's doing in theory, but the actual matrix manipulation bit is kinda opaque)
+		float[] buffX = new float[(int)(mapX.total() * mapX.channels())];
+		mapX.get(0, 0, buffX);
+		float[] buffY = new float[(int)(mapY.total() * mapY.channels())];
+		mapY.get(0, 0, buffY);
+
+		for(int i = 0; i < mapX.rows(); i++){
+			for(int j = 0; j < mapX.cols(); j++){
+				buffX[i * mapX.cols() + j] = mapY.cols() - j;
+				buffY[i * mapY.cols() + j] = i;
+			}
+		}
+
+		mapX.put(0, 0, buffX);
+		mapY.put(0, 0, buffY);
+	}
+
+	/**
+	 * Processes the given source image using the given operation, and returns the resulting destination image. This
+	 * method eliminates the need for explicit creation of a new destination image variable each time, allowing each
+	 * operation to be condensed to a one-liner, much like {@code String} manipulation. For example, a typical call
+	 * might look like this:
+	 * <p></p>
+	 * <center>{@code frame = process(frame, (s, d) -> Imgproc.resize(s, d, new Size(width, height)));}</center>
+	 * @param src The source image to be processed
+	 * @param operation A {@link BiConsumer} representing the operation to be performed, usually expressed as a lambda
+	 *                  expression.
+	 * @return The resulting destination image. This may be assigned to the source image variable to overwrite it.
+	 */
+	public static Mat process(Mat src, BiConsumer<Mat, Mat> operation){
+		Mat dest = Mat.zeros(1, 1, CvType.CV_8UC1);
+		operation.accept(src, dest);
+		return dest;
+	}
+
+}
