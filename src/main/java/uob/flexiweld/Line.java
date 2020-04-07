5d8e6fca1487b98cde9de3735687c29a31efeda7
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2020-04-07 18:20:57 +0100

    Implement detection of intersections and angle readout at each intersection, and fix issues with vertical lines

--- src/main/java/uob/flexiweld/Line.java
+++ src/main/java/uob/flexiweld/Line.java
@@ -68,13 +68,21 @@ public class Line {
 		return new Point(end.x - start.x, end.y - start.y);
 	}
 
+	/** Returns the gradient of this line. For vertical or almost-vertical lines, the returned gradient is infinite. */
+	public double gradient(){
+		double dx = (end.x - start.x);
+		double dy = (end.y - start.y);
+		return dy/dx;
+	}
+
 	/** Returns the perpendicular distance from this line to the given point. */
 	public double distanceTo(Point point){
 		Line hypot = new Line(this.start, point);
 		return hypot.length() * Math.sin(acuteAngleBetween(this, hypot));
 	}
 
-	/** Returns the point on this line that is nearest to the given point. */
+	/** Returns the point on this line that is nearest to the given point. The returned point may be beyond either end
+	 * of the line. */
 	public Point nearestPointTo(Point point){
 		Line hypot = new Line(this.start, point);
 		// Yes this should be angleBetween and not acuteAngleBetween, this time direction matters!
@@ -109,6 +117,37 @@ public class Line {
 		return new Line(start, end);
 	}
 
+	/**
+	 * Returns a copy of this line, extended equally in both directions such that the overall length is increased by
+	 * the given fraction.
+	 * @param fraction The fraction by which to extend the length of the line. Negative values will shorten the line.
+	 * @return The resulting extended line. The length of this line will be equal to the length of the original line
+	 * multiplied by {@code (1 + fraction)}. If this results in a negative length, the returned line
+	 * will be in the opposite direction to the original ({@code line.extended(-2)} is therefore equivalent to
+	 * {@code line.reversed()}).
+	 */
+	public Line extended(float fraction){
+		return this.extended(fraction/2, fraction/2);
+	}
+
+	/**
+	 * Returns a copy of this line, extended by the given fraction at each end.
+	 * @param startFraction The fraction by which to extend the length of the line beyond its start point. Negative
+	 *                      values will shorten the line.
+	 * @param endFraction The fraction by which to extend the length of the line beyond its end point. Negative
+	 *                    values will shorten the line.
+	 * @return The resulting extended line. The length of this line will be equal to the length of the original line
+	 * multiplied by {@code (1 + startFraction + endFraction)}. If this results in a negative length, the returned line
+	 * will be in the opposite direction to the original ({@code line.extended(-1, -1)} is therefore equivalent to
+	 * {@code line.reversed()}).
+	 */
+	public Line extended(float startFraction, float endFraction){
+		double dx = end.x - start.x;
+		double dy = end.y - start.y;
+		return new Line(start.x - dx * startFraction, start.y - dy * startFraction,
+						end.x   + dx * endFraction,   end.y   + dy * endFraction);
+	}
+
 	// Binary operations
 
 	/**
@@ -120,8 +159,8 @@ public class Line {
 	 * @return The resulting line.
 	 */
 	public static Line equidistant(Line l, Line m){
-		l = l.xRectified();
-		m = m.xRectified();
+		l = Math.abs(l.gradient()) > 1 ? l.yRectified() : l.xRectified();
+		m = Math.abs(m.gradient()) > 1 ? m.yRectified() : m.xRectified();
 		return new Line((l.start.x + m.start.x) / 2, (l.start.y + m.start.y) / 2,
 				(l.end.x + m.end.x) / 2, (l.end.y + m.end.y) / 2);
 	}
@@ -143,6 +182,75 @@ public class Line {
 	public static double obtuseAngleBetween(Line l, Line m){
 		double angleDiff = angleBetween(l, m);
 		return Math.max(angleDiff, Math.PI - angleDiff);
+	}
+
+	/** Returns the point where the two given lines intersect, or null if they do not intersect or are coincident. */
+	public static Point intersection(Line l, Line m){
+
+		// Some sort of dark magic with matrix determinants, I don't know how it avoids divide by 0 issues for vertical
+		// lines without resorting to trig but it does
+		// https://rosettacode.org/wiki/Find_the_intersection_of_two_lines#Java (for some reason this has the dxs backwards)
+		// https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line
+		// https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+
+//		double dy1 = l.end.y - l.start.y;
+//		double dx1 = l.end.x - l.start.x;
+//		double c1 = dy1 * l.start.x - dx1 * l.start.y;
+//
+//		double dy2 = m.end.y - m.start.y;
+//		double dx2 = m.end.x - m.start.x;
+//		double c2 = dy2 * m.start.x - dx2 * m.start.y;
+//
+//		double det = dy2 * dx1 - dy1 * dx2;
+//		if(det == 0) return null;
+//		return new Point((dx1 * c2 - dx2 * c1) / det, (dy1 * c2 - dy2 * c1) / det);
+
+		// Parametric version
+		double dy1 = l.end.y - l.start.y;
+		double dx1 = l.end.x - l.start.x;
+
+		double dy2 = m.end.y - m.start.y;
+		double dx2 = m.end.x - m.start.x;
+
+		// The imaginary third line connects the start points of both lines
+		double dy3 = m.start.y - l.start.y;
+		double dx3 = m.start.x - l.start.x;
+
+		double det = dy2 * dx1 - dy1 * dx2;
+		if(det == 0) return null; // Lines are parallel or coincident
+
+		// t is the fraction along line l, u is the fraction along line m
+		// Therefore both t and u must be between 0 and 1 (inclusive) if the lines intersect
+		double t = (dx3 * dy2 - dy3 * dx2) / det;
+		double u = (dx3 * dy1 - dy3 * dx1) / det;
+
+		if(t < 0 || t > 1 || u < 0 || u > 1) return null; // Intersection is beyond the end of one or both of the lines
+
+		return new Point(l.start.x + t * dx1, l.start.y + t * dy1);
+
+		// Standard linear algebra should be avoided because it is tied to the coordinate system, so vertical and
+		// near-vertical lines must be accounted for separately to avoid doubles overflowing to infinity
+		// https://stackoverflow.com/questions/4543506/algorithm-for-intersection-of-2-lines
+
+//		l = l.xRectified();
+//		m = m.xRectified();
+//
+//		double lGradient = l.gradient();
+//		double mGradient = m.gradient();
+//
+//		if(lGradient == mGradient) return null; // The lines are parallel or coincident
+//
+//		// Simultaneous equations yay
+//		double x = (l.start.y - m.start.y + mGradient * m.start.x - lGradient * l.start.x) / (mGradient - lGradient);
+//		double y = l.start.y + lGradient * (x - l.start.x);
+//
+//		if(x < l.start.x || x > l.end.x || x < m.start.x || x > m.end.x) return null; // Intersection is beyond the end
+//
+//		return new Point(x, y);
+
+		// I did figure out a third way of doing it using trig, it's probably horribly inefficient but it avoids
+		// issues with infinity by working with angles instead of gradients
+
 	}
 
 }
--- src/main/java/uob/flexiweld/LineDetectorTest.java
+++ src/main/java/uob/flexiweld/LineDetectorTest.java
@@ -20,15 +20,26 @@ public class LineDetectorTest {
 
 	public static final int FRAMERATE = 30; // Target framerate in frames per second
 
-	public static final double ANGLE_THRESHOLD = Math.toRadians(10); // Lines within 2 degrees are considered parallel
+	/** Lines within this angle of each other are considered parallel */
+	public static final double ANGLE_THRESHOLD = Math.toRadians(10);
+	/** Parallel lines within this distance of each other are considered coincident */
 	public static final double DISTANCE_THRESHOLD = 10;
+	/** Pairs of parallel, non-coincident lines within this distance of each other are considered to be tubes */
+	public static final double WIDTH_THRESHOLD = 200; // Excludes e.g. the edges of the test card
 
-	public static final int BORDER = 20;
+	public static final int BORDER = 150;
 
 	public static final Scalar WHITE = new Scalar(255, 255, 255); // This is blue-green-red for some reason
+	public static final Scalar BLACK = new Scalar(0, 0, 0);
+	public static final Scalar RED = new Scalar(0, 0, 255);
 	public static final Scalar GREEN = new Scalar(0, 255, 0);
+	public static final Scalar BLUE = new Scalar(255, 0, 0);
 	public static final Scalar CYAN = new Scalar(255, 255, 0);
+	public static final Scalar MAGENTA = new Scalar(255, 0, 255);
 	public static final Scalar YELLOW = new Scalar(0, 255, 255);
+
+	public static final double ANGLE_RADIUS = 60;
+	private static final Size ELLIPSE_SIZE = new Size(ANGLE_RADIUS - 10, ANGLE_RADIUS - 10);
 
 	public static final int INTERP_FRAMES = 5; // Number of frames to average over for tube detection
 
@@ -91,25 +102,7 @@ public class LineDetectorTest {
 
 			if(displayLines){
 
-				// Probabilistic Hough Line Transform
-				Mat lineMatrix = new Mat(); // will hold the results of the detection
-				Imgproc.HoughLinesP(edges, lineMatrix, 1, Math.PI / 180, 100, 50, 100); // runs the actual detection
-
-				List<Line> lines = new ArrayList<>();
-
-				// Draw the lines
-				for(int i = 0; i < lineMatrix.rows(); i++){
-
-					double[] l = lineMatrix.get(i, 0);
-					// Discard lines that are on the edge of the frame, we don't want to detect the edge
-					if(l[0] < BORDER && l[2] < BORDER || l[0] > frame.width() - BORDER && l[2] > frame.width() - BORDER
-					|| l[1] < BORDER && l[3] < BORDER || l[1] > frame.height() - BORDER && l[3] > frame.height() - BORDER)
-						continue;
-					// Create the line, add to the list and draw it
-					Line line = new Line(l[0], l[1], l[2], l[3]);
-					lines.add(line);
-					//Imgproc.line(frame, line.getStart(), line.getEnd(), GREEN, 2, Imgproc.LINE_AA, 0);
-				}
+				List<Line> lines = extractLines(edges, BORDER);
 
 				lines.sort(Comparator.comparing(Line::angle));
 
@@ -120,61 +113,52 @@ public class LineDetectorTest {
 				List<Line> allPrevLines = new ArrayList<>(Utils.flatten(prevLines));
 				Collections.reverse(allPrevLines); // Do the more recent lines first
 
-				List<Line> averagedLines = new ArrayList<>();
+				List<Line> averagedLines = fuzzyAverageLines(allPrevLines, DISTANCE_THRESHOLD, ANGLE_THRESHOLD);
 
-				while(!allPrevLines.isEmpty()){
+				for(Line line : averagedLines) Imgproc.line(frame, line.getStart(), line.getEnd(), BLUE, 2, Imgproc.LINE_AA, 0);
 
-					Line ref = allPrevLines.get(0);
+				List<Line> centrelines = findCentrelines(averagedLines, WIDTH_THRESHOLD, ANGLE_THRESHOLD);
+				centrelines.sort(Comparator.comparing(Line::angle).reversed());
 
-					List<Line> coincident = new ArrayList<>();
+				for(Line line : centrelines) Imgproc.line(frame, line.getStart(), line.getEnd(), CYAN, 2, Imgproc.LINE_AA, 0);
 
-					for(Line line : allPrevLines){
-						// Determine if each line should be considered coincident with ref, and if so add to the list
-						if(ref.distanceTo(line.midpoint()) < DISTANCE_THRESHOLD && Line.acuteAngleBetween(ref, line) < ANGLE_THRESHOLD){
-							coincident.add(line);
+				for(int i = 0; i < centrelines.size(); i++){
+
+					Line lineA = centrelines.get(i);
+
+					for(int j = i; j < centrelines.size(); j++){
+
+						Line lineB = centrelines.get(j);
+
+						Point intersection = Line.intersection(lineA, lineB);
+
+						if(intersection != null){
+
+							// Sorting by angle earlier seems to mean line B always has the smaller angle
+							double startAngle = lineB.angle();
+							double endAngle = lineA.angle();
+							double angle = endAngle - startAngle;
+
+							if(angle > Math.PI/2){
+								double prevStartAngle = startAngle;
+								startAngle = endAngle - Math.PI;
+								endAngle = prevStartAngle;
+								angle = endAngle - startAngle;
+							}
+
+							double midAngle = (startAngle + endAngle) / 2; // Should be fine since centrelines are rectified
+
+							Imgproc.drawMarker(frame, intersection, YELLOW, Imgproc.MARKER_CROSS, 14, 2);
+
+							Imgproc.ellipse(frame, intersection, ELLIPSE_SIZE, 0, Math.toDegrees(startAngle),
+									Math.toDegrees(endAngle), YELLOW, 2);
+
+							Imgproc.putText(frame, String.format("%.2fdeg", Math.toDegrees(angle)),
+									new Point(intersection.x + ANGLE_RADIUS * Math.cos(midAngle),
+											intersection.y  + ANGLE_RADIUS * Math.sin(midAngle) + 10),
+									Core.FONT_HERSHEY_PLAIN, 2, YELLOW, 2);
 						}
 					}
-
-					allPrevLines.removeAll(coincident);
-
-					List<Point> starts = coincident.stream().map(l -> l.xRectified().getStart()).collect(Collectors.toList());
-					List<Point> ends = coincident.stream().map(l -> l.xRectified().getEnd()).collect(Collectors.toList());
-
-					Line averageLine = new Line(starts.stream().mapToDouble(p -> p.x).average().orElse(0),
-												starts.stream().mapToDouble(p -> p.y).average().orElse(0),
-												ends.stream().mapToDouble(p -> p.x).average().orElse(0),
-												ends.stream().mapToDouble(p -> p.y).average().orElse(0));
-
-					// Most extreme ends of lines - since we x-rectified them these are simply the max/min x coords
-					Point start = starts.stream().min(Comparator.comparingDouble(p -> p.x)).orElse(null);
-					Point end = ends.stream().max(Comparator.comparingDouble(p -> p.x)).orElse(null);
-
-					averageLine = new Line(averageLine.nearestPointTo(start), averageLine.nearestPointTo(end));
-
-					averagedLines.add(averageLine);
-
-					Imgproc.line(frame, averageLine.getStart(), averageLine.getEnd(), CYAN, 2, Imgproc.LINE_AA, 0);
-
-				}
-
-				List<Line> centrelines = new ArrayList<>();
-
-				for(int i=0; i<averagedLines.size(); i++){
-					Line lineA = averagedLines.get(i);
-					Line lineB = averagedLines.get((i+1) % averagedLines.size());
-					if(Line.acuteAngleBetween(lineA, lineB) < ANGLE_THRESHOLD){
-						Line centreline = Line.equidistant(lineA, lineB);
-						centrelines.add(centreline);
-						Imgproc.line(frame, centreline.getStart(), centreline.getEnd(), YELLOW, 2, Imgproc.LINE_AA, 0);
-					}
-				}
-
-				// Super quick fudge to try and read an angle
-				if(centrelines.size() >= 2){
-					Line lineA = centrelines.get(0);
-					Line lineB = centrelines.get(1);
-					Imgproc.putText(frame, String.format("%.2f deg", Math.toDegrees(Line.acuteAngleBetween(lineA, lineB))),
-							lineA.getStart(), Core.FONT_HERSHEY_PLAIN, 2, WHITE);
 				}
 
 			}
@@ -203,6 +187,119 @@ public class LineDetectorTest {
 
 		HighGui.destroyAllWindows();
 		System.exit(0);
+	}
+
+	/**
+	 * Performs a probabilistic Hough line transform on the given image source and converts the resulting matrix to a
+	 * list of {@link Line} objects.
+	 * @param source The image from which lines are to be extracted, usually the output of an edge detection filter
+	 * @param border Detected lines whose ends both lie within this distance of the edge of the image will be ignored
+	 * @return The resulting list of {@code Line} objects (in the order returned by the Hough line transform)
+	 */
+	private static List<Line> extractLines(Mat source, int border){
+
+		// Probabilistic Hough Line Transform
+		Mat lineMatrix = new Mat(); // will hold the results of the detection
+		Imgproc.HoughLinesP(source, lineMatrix, 1, Math.PI / 180, 100, 50, 100); // runs the actual detection
+
+		List<Line> lines = new ArrayList<>();
+
+		// Draw the lines
+		for(int i = 0; i < lineMatrix.rows(); i++){
+
+			double[] l = lineMatrix.get(i, 0);
+			// Discard lines that are on the edge of the frame, we don't want to detect the edge
+			if(l[0] < border && l[2] < border || l[0] > source.width() - border && l[2] > source.width() - border
+			|| l[1] < border && l[3] < border || l[1] > source.height() - border && l[3] > source.height() - border)
+				continue;
+			// Create the line, add to the list and draw it
+			Line line = new Line(l[0], l[1], l[2], l[3]);
+			lines.add(line);
+			//Imgproc.line(frame, line.getStart(), line.getEnd(), GREEN, 2, Imgproc.LINE_AA, 0);
+		}
+
+		return lines;
+	}
+
+	/**
+	 * Performs a 'fuzzy average' of the given list of lines. Lines within the given distance and angle thresholds of
+	 * each other are considered coincident and are merged by averaging their start and end points.
+	 * @param lines A list of {@link Line} objects to be averaged (will not be modified by this method)
+	 * @param distThreshold The maximum distance between two lines for them to be considered coincident (the distance
+	 *                      between two lines is taken as the smaller of the perpendicular distances from one line to
+	 *                      the midpoint of the other)
+	 * @param angleThreshold The maximum (acute) angle between two lines for them to be considered coincident
+	 * @return The resulting list of averaged lines
+	 */
+	private static List<Line> fuzzyAverageLines(List<Line> lines, double distThreshold, double angleThreshold){
+
+		List<Line> averagedLines = new ArrayList<>();
+
+		while(!lines.isEmpty()){
+
+			Line ref = lines.get(0);
+			// Choose which axis to rectify/compare in based on the gradient of the reference line
+			// Avoids issues with vertical lines
+			final boolean compareYs = Math.abs(ref.gradient()) > 1;
+			lines = lines.stream().map(compareYs ? Line::yRectified : Line::xRectified).collect(Collectors.toList());
+
+			List<Line> coincident = new ArrayList<>();
+
+			for(Line line : lines){
+				// Determine if each line should be considered coincident with ref, and if so add to the list
+				if(ref.distanceTo(line.midpoint()) < distThreshold && Line.acuteAngleBetween(ref, line) < angleThreshold){
+					coincident.add(line);
+				}
+			}
+
+			lines.removeAll(coincident);
+
+			// Could probably replace all of this with regression or something similar
+
+			List<Point> starts = coincident.stream().map(Line::getStart).collect(Collectors.toList());
+			List<Point> ends = coincident.stream().map(Line::getEnd).collect(Collectors.toList());
+
+			Line averageLine = new Line(starts.stream().mapToDouble(p -> p.x).average().orElse(0),
+					starts.stream().mapToDouble(p -> p.y).average().orElse(0),
+					ends.stream().mapToDouble(p -> p.x).average().orElse(0),
+					ends.stream().mapToDouble(p -> p.y).average().orElse(0));
+
+			// Most extreme ends of lines - take the sum of x and y coords to avoid issues with vertical lines
+			Point start = starts.stream().min(Comparator.comparingDouble(p -> compareYs ? p.y : p.x)).orElse(null);
+			Point end = ends.stream().max(Comparator.comparingDouble(p -> compareYs ? p.y : p.x)).orElse(null);
+
+			averageLine = new Line(averageLine.nearestPointTo(start), averageLine.nearestPointTo(end));
+
+			averagedLines.add(averageLine);
+		}
+
+		return averagedLines;
+	}
+
+	/**
+	 * Detects pairs of (approximately) parallel lines that are less than the specified width apart and returns a list
+	 * of lines equidistant from both lines of each pair.
+	 * @param lines A list of {@link Line} objects to find centrelines for (will not be modified by this method)
+	 * @param widthThreshold The maximum distance between pairs of lines for a centreline to be detected
+	 * @param angleThreshold The maximum angle between pairs of lines for a centreline to be detected
+	 * @return The resulting list of centrelines
+	 */
+	private static List<Line> findCentrelines(List<Line> lines, double widthThreshold, double angleThreshold){
+
+		List<Line> centrelines = new ArrayList<>();
+
+		for(int i=0; i<lines.size(); i++){
+
+			Line lineA = lines.get(i);
+			Line lineB = lines.get((i+1) % lines.size());
+
+			if(lineA.distanceTo(lineB.midpoint()) < widthThreshold && Line.acuteAngleBetween(lineA, lineB) < angleThreshold){
+				Line centreline = Line.equidistant(lineA, lineB).extended(0.25f);
+				centrelines.add(centreline);
+			}
+		}
+
+		return centrelines;
 	}
 
 }
