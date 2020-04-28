56c593860881da35640cf011ede48b5131389db6
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2020-04-28 15:20:30 +0100

    Implement snapshot function

--- src/main/java/uob/flexiweld/app/LiveMode.java
+++ src/main/java/uob/flexiweld/app/LiveMode.java
@@ -1,9 +1,13 @@
 package uob.flexiweld.app;
 
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 
 public abstract class LiveMode extends CaptureMode {
@@ -20,7 +24,7 @@ public abstract class LiveMode extends CaptureMode {
 		JToggleButton pauseButton = FlexiweldApp.createToggleButton("\u275a\u275a Pause", e -> app.getVideoFeed().togglePause());
 		pauseButton.setSelected(app.getVideoFeed().isPaused());
 		components.add(pauseButton);
-		components.add(FlexiweldApp.createButton("\ud83d\udcf7 Snapshot", e -> {}));
+		components.add(FlexiweldApp.createButton("\ud83d\udcf7 Snapshot", e -> takeSnapshot(app)));
 	}
 
 	@Override
@@ -33,6 +37,29 @@ public abstract class LiveMode extends CaptureMode {
 	public Mat processFrame(VideoFeed videoFeed, Mat raw){
 		fpsReadout.setText(String.format("%.4g fps", videoFeed.getFps()));
 		return raw; // Do nothing
+	}
+
+	private void takeSnapshot(FlexiweldApp app){
+
+		app.getVideoFeed().pauseFor(1000);
+
+		JFileChooser imageExporter = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("JPEG images", "jpg", "jpeg", "jfif");
+		imageExporter.setFileFilter(filter);
+
+		int result = imageExporter.showSaveDialog(app.getFrame());
+
+		if(result == JFileChooser.APPROVE_OPTION){
+
+			File file = imageExporter.getSelectedFile();
+			String path = file.getPath();
+
+			if(Arrays.stream(filter.getExtensions()).noneMatch(s -> file.getName().endsWith(s))){
+				path = path + ".jpg";
+			}
+
+			Imgcodecs.imwrite(path, app.getVideoFeed().getCurrentFrame());
+		}
 	}
 
 }
