37889177ecce61ac294cffedfd949e3b7fbdad1a
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2020-05-06 17:43:18 +0100

    Comments

--- src/main/java/uob/flexiweld/app/CaptureMode.java
+++ src/main/java/uob/flexiweld/app/CaptureMode.java
@@ -6,7 +6,16 @@ import java.awt.*;
 import java.util.List;
 
 /**
- * Abstract base class for all capture modes. This class
+ * Abstract base class for all capture modes. One instance of this class is active at any one time; this instance is
+ * stored in {@code FlexiweldApp#mode}. Capture modes are responsible for:
+ * <p></p>
+ * - Defining the buttons displayed on the control panel, via {@link CaptureMode#populateControls(FlexiweldApp, List)}<br>
+ * - Defining the buttons displayed on the control panel, via {@link CaptureMode#populateStatusBar(List)}<br>
+ * - Performing the necessary processing steps on each raw frame of the video feed, via
+ * {@link CaptureMode#processFrame(VideoFeed, Mat)}<br>
+ * - Adding any required annotations or post-processing to each processed, scaled frame of the video feed, via
+ * {@link CaptureMode#annotateFrame(VideoFeed, Mat)}<br>
+ * @author Finin Quincey
  */
 // N.B. If we're getting technical, this is the *state interface* part of a *state pattern*.
 // This is an abstract class rather than an interface because certain things are shared by all modes
@@ -44,8 +53,8 @@ public abstract class CaptureMode {
 	 * @param videoFeed The video feed object calling this method, for reference.
 	 * @param frame The raw video frame, as captured by the camera, with no distortion correction, scaling or other
 	 *            modifications. Since some OpenCV methods modify images directly and others require a destination
-	 *            matrix, implementors are free to decide whether to modify the raw frame - the {@link Mat} object will
-	 *            not be used until the next frame, when it will be overwritten entirely.
+	 *            matrix, implementors are free to decide whether to modify this parameter. The {@link Mat} object
+	 *            itself will be overwritten next frame, so if it is to be stored, a copy should be made first.
 	 * @return The resulting frame, after processing
 	 */
 	public abstract Mat processFrame(VideoFeed videoFeed, Mat frame);
@@ -57,8 +66,8 @@ public abstract class CaptureMode {
 	 * @param videoFeed The video feed object calling this method, for reference.
 	 * @param frame The processed video frame, after scaling, flipping, distortion correction and other
 	 *            modifications. Since some OpenCV methods modify images directly and others require a destination
-	 *            matrix, implementors are free to decide whether to modify the frame - the {@link Mat} object will
-	 *            not be used until the next frame, when it will be overwritten entirely.
+	 *            matrix, implementors are free to decide whether to modify this parameter. The {@link Mat} object
+	 *            itself will be overwritten next frame, so if it is to be stored, a copy should be made first.
 	 * @return The resulting frame, after annotations have been drawn
 	 */
 	public abstract Mat annotateFrame(VideoFeed videoFeed, Mat frame);
